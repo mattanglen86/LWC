b3b4411c2ae7004ab2c868a4ebf7be57e11da941
mattanglen86 <matt.p.anglen@gmail.com>
2021-10-01 08:14:17 -0600

    Basic lwc testing

--- force-app/main/default/lwc/unitTest/__tests__/sum.test.js
+++ force-app/main/default/lwc/unitTest/__tests__/sum.test.js
@@ -1,6 +1,9 @@
 import { sum } from '../sum';
-
+//takes in a string to describe the test, and a callback function
 describe('sum()', () => {
+    //this is the test itself "it" is an alias
+    //expect is the actual method and toBe is what we expect
+    //ie System.assertEquals(".toBe", "expect(method())")
     it('should add 1 and 2 returning 3', () => {
         expect(sum(1,2)).toBe(3);
     });
--- force-app/main/default/lwc/unitTest/__tests__/unitTest.test.js
+++ force-app/main/default/lwc/unitTest/__tests__/unitTest.test.js
@@ -2,12 +2,13 @@ import { createElement } from 'lwc';
 import UnitTest from 'c/unitTest';
 
 describe('c-unit-test', () => {
+    //Clears DOM before first and after each test - like refreshing browser
     afterEach(() => {
         while(document.body.firstChild) {
             document.body.removeChild(document.body.firstChild);
         }
     });
-
+    //adds an element to LWC, testing that the component will display
     it('displays unit status with default number', () => {
         const element = createElement('c-unit-test', {
             is : UnitTest
@@ -17,7 +18,8 @@ describe('c-unit-test', () => {
         const div = element.shadowRoot.querySelector('div');
         expect(div.textContent).toBe('Unit 5 alive!');
     });
-
+    //changes the display, confirming component can be updated
+    //changes in status require Promise (async) so test can wait
     it('displays unit status with updated unitNumber', () => {
         const element = createElement('c-unit-test', {
             is : UnitTest
@@ -30,7 +32,7 @@ describe('c-unit-test', () => {
             expect(div.textContent).toBe('Unit 6 alive!');
         });
     });
-
+    //tests user input, also requires Promise (async) due to status change
     it('displays unit status with input change event', () => {
         const element = createElement('c-unit-test', {
             is : UnitTest
